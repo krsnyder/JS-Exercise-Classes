2565c98d62535b15665ba73ae02d0fdfe4c03b5c
Kirk <kirkrobertsnyder@gmail.com>
2021-01-28 11:31:07 -0500

    Task 2

--- index.js
+++ index.js
@@ -75,9 +75,27 @@ class Person {
   */
   
  class Car {
-    
+  constructor(model, milesPerGallon) {
+   this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
   }
-  
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    if (this.tank > (distance / this.milesPerGallon)) {
+    this.tank -= (distance / this.milesPerGallon);
+    this.odometer += distance;
+    } else {
+    this.odometer += this.tank * this.milesPerGallon;
+    this.tank = 0;
+    return `"I ran out of fuel at ${this.odometer} miles!"`
+    }
+  }
+}
+
   /*
     TASK 3
       - Write a Lambdasian class.
